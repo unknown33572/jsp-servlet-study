aebf400dde75594351a3bd655408fdca8132c84e
S <unknown33572@gmail.com>
2023-10-09 21:53:06 +0900

    servlet-example

--- src/main/java/com/study/HelloServlet.java
+++ src/main/java/com/study/HelloServlet.java
@@ -1,2 +1,18 @@
-package com.study;public class HelloServlet {
+package com.study;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    resp.setContentType("text/html; charset=utf-8");
+    PrintWriter out = resp.getWriter();
+    out.println("<h1>Hello Servlet</h1>");
+    out.close();
+  }
 }
